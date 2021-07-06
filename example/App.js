1234f32ebc1563748d37b3d8d685966e742b83f3
洛竹 <youngjuning@aliyun.com>
2021-07-07 01:25:49 +0800

    Init Commit

---
+++ example/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import Button from '@luozhu/button';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Button />
+      </View>
+    );
+  }
+}
---
+++ packages/button/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Button } from 'react-native'
+
+export default class Button1 extends Component {
+  render() {
+    return (
+      <View>
+        <Button title="hah"/>
+      </View>
+    )
+  }
+}
